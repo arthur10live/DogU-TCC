86289fcfd48dca418f2b62034914ae5adcfe8ae2
Arthur Henrique - Desktop <arthurhenriquec6@live.com>
2019-09-05 04:00:06 -0300

    Revert "config site"
    This reverts commit 82087e294db8d274997e6b068e0498406ff997af.

--- config/conecta_banco.php
+++ config/conecta_banco.php
@@ -1,14 +1,26 @@
 <?php 
+$con_ip = "sql255.main-hosting.eu";
+//$con_ip = "localhost";
 $con_login = "u765863036_dogu";
 //$con_login = "root";
 $con_senha = "123123asd";
 //$con_senha = "usbw";
+$con_db = "u765863036_dogu";
+$conexao = mysqli_connect($con_ip, $con_login, $con_senha, $con_db);
 
-try {
-    $conexao = new PDO('mysql:host=sql255.main-hosting.eu;dbname=u765863036_dogu', $con_login, $con_senha);
-    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-      echo 'ERROR: ' . $e->getMessage();
+//Se der erro, mensagem:
+if (mysqli_connect_errno())
+{
+    echo "A conexão MySQLi apresentou erro: " . mysqli_connect_error();
 }
 
+//O tipo de caracteres a ser usado
+header('Content-Type: text/html; charset=utf-8');
+
+//Depois da tua conexão a base de dados insere o seguinte código abaixo.
+//Esta parte vai resolver o teu problema!
+ mysql_query("SET NAMES 'utf8'");
+ mysql_query('SET character_set_connection=utf8');
+ mysql_query('SET character_set_client=utf8');
+ mysql_query('SET character_set_results=utf8');
 ?> 
