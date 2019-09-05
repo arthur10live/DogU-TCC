1a6efbc8aeaa10c458706198d3681487884eb168
Arthur Henrique - Desktop <arthurhenriquec6@live.com>
2019-09-05 04:17:47 -0300

    config web

--- config/conecta_banco.php
+++ config/conecta_banco.php
@@ -1,10 +1,10 @@
 <?php 
-$con_ip = "sql255.main-hosting.eu";
-//$con_ip = "localhost";
-$con_login = "u765863036_dogu";
-//$con_login = "root";
-$con_senha = "123123asd";
-//$con_senha = "usbw";
+//$con_ip = "sql255.main-hosting.eu";
+$con_ip = "localhost";
+//$con_login = "u765863036_dogu";
+$con_login = "arthur";
+//$con_senha = "123123asd";
+$con_senha = "0607";
 $con_db = "u765863036_dogu";
 $conexao = mysqli_connect($con_ip, $con_login, $con_senha, $con_db);
 
@@ -16,11 +16,4 @@ if (mysqli_connect_errno())
 
 //O tipo de caracteres a ser usado
 header('Content-Type: text/html; charset=utf-8');
-
-//Depois da tua conexão a base de dados insere o seguinte código abaixo.
-//Esta parte vai resolver o teu problema!
- mysql_query("SET NAMES 'utf8'");
- mysql_query('SET character_set_connection=utf8');
- mysql_query('SET character_set_client=utf8');
- mysql_query('SET character_set_results=utf8');
 ?> 
