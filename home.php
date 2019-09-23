ac7381f52d7c331773fb72e9a8cc1faa03c7902e
Ariane Leite <45472771+leiteav@users.noreply.github.com>
2019-09-22 22:05:42 -0300

    ajustes home

--- home.php
+++ home.php
@@ -190,10 +190,10 @@
                             </div>
                         </div>
                     </section>
-                    <section class="col-md-7 chocolate" >
-                                            
-                                                
-                    <h3 style="margin-top: 100px;">
+                    <section class="col-md-7 chocolate">
+
+
+                        <h3 style="margin-top: 100px;">
                             Passear com seu cão além de gastar sua energia e acalmá-lo também pode trazer diversos
                             outros benefícios como ensinar seu cão a ter paciência e não sair correndo e puxando a
                             coleira, o ajuda também a socializar com outros cães e aprender a respeitar o espaço do
@@ -254,7 +254,7 @@
     <!-- JQUERY / POPPER / BOOTSTRAP.JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
- 
+
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
@@ -263,8 +263,9 @@
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
 
-    <script type="text/javascript" scr="js/jquery-3.4.1.min.js"></script>
-    
+    <script type="text/javascript" scr="js/jquery-3.4.1.min.js">
+    </script>
+
 
 </body>
 
