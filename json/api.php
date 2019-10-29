e9b3430a0a1cd8c1ead08942c8d68a54db1ed1a3
Arthur Henrique - Desktop <arthurhenriquec6@live.com>
2019-10-29 17:52:43 -0300

    Update api.php

--- json/api.php
+++ json/api.php
@@ -16,12 +16,6 @@ if (mysqli_connect_errno())
     $obj = json_decode($obj, true); // transformar json em objeto
 
     if($obj['action'] == "logar"){
-      /*if(
-        $obj['email']) == ""||
-        $obj['senha']) == ""
-      ){
-        $results = array('error' => 'Preencha todos os campos!');
-      }else{*/
         $email = mysqli_real_escape_string($conexao, $obj['email']);
         $senha = mysqli_real_escape_string($conexao, $obj['senha']);
         $sql = "SELECT cd_login FROM tb_login WHERE cd_email = '$email' AND cd_senha = MD5('$senha') LIMIT 1;";
@@ -32,42 +26,74 @@ if (mysqli_connect_errno())
         }else{	
             $results = array('error' => 'Credenciais Incorretas!');
         }
-      /*}*/
     }else if($obj['action'] == "cadastrar"){
-        if (
-          $obj['cpf'] == "" ||
-          $obj['rg'] == "" ||
-          $obj['nome'] == "" ||
-          $obj['email'] == "" ||
-          $obj['senha'] == "" ||
-          $obj['nascimento'] == ""
-        ){
-          $results = array('sucesso' => 0);
-        }else{
-          $cpf = mysqli_real_escape_string($conexao, $obj['cpf']);
-          $rg = mysqli_real_escape_string($conexao, $obj['rg']);
-          $nome = mysqli_real_escape_string($conexao, $obj['nome']);
-          $email = mysqli_real_escape_string($conexao, $obj['email']);
-          $senha = mysqli_real_escape_string($conexao, $obj['senha']);
-          $nascimento = mysqli_real_escape_string($conexao, $obj['nascimento']);
-          $sql = "CALL criarUsuario( '$cpf','$rg','$nome','$email','$senha','$nascimento');";
-          try{
-            mysqli_query($conexao, $sql);
-          } catch (Exception $e){}
-          $sql = "SELECT cd_login FROM tb_login WHERE cd_email = '$email' AND cd_senha = MD5('$senha') LIMIT 1;";
-          $resultado_usuario = mysqli_query($conexao, $sql);
-          $resultado = mysqli_fetch_assoc($resultado_usuario);
-          if(isset($resultado)){
-              $results = array('sucesso' => 1);
-          }else{	
-              $results = array('sucesso' => 0);
+        if (!verificarLogin($obj['email'])) {
+          if (!verificarRG($obj['rg'])){
+            if (!verificarRG($obj['cpf'])){
+              $cpf = mysqli_real_escape_string($conexao, $obj['cpf']);
+              $rg = mysqli_real_escape_string($conexao, $obj['rg']);
+              $nome = mysqli_real_escape_string($conexao, $obj['nome']);
+              $email = mysqli_real_escape_string($conexao, $obj['email']);
+              $senha = mysqli_real_escape_string($conexao, $obj['senha']);
+              $nascimento = mysqli_real_escape_string($conexao, $obj['nascimento']);
+              $sql = "CALL criarUsuario( '$cpf','$rg','$nome','$email','$senha','$nascimento');";
+              try{
+                mysqli_query($conexao, $sql);
+              } catch (Exception $e){}
+              $sql = "SELECT cd_login FROM tb_login WHERE cd_email = '$email' AND cd_senha = MD5('$senha') LIMIT 1;";
+              $resultado_usuario = mysqli_query($conexao, $sql);
+              $resultado = mysqli_fetch_assoc($resultado_usuario);
+              if(isset($resultado)){
+                  $results = array('sucesso' => 1);
+              }else{	
+                  $results = array('sucesso' => 0, 'msg' => 'Erro ao cadastrar usuário!');
+              }
+            }else{
+              $results = array('sucesso' => 0, 'msg' => 'CPF já cadastrado!');
+            }
+          }else{
+            $results = array('sucesso' => 0, 'msg' => 'RG já cadastrado!');
           }
-        }
-    } else{
+        }else{
+          $results = array('sucesso' => 0, 'msg' => 'E-mail já cadastrado!');
+        } 
+      } else{
         $results = array('error' => 'Função não encontrada!');
     }
-}
 
 echo json_encode($results);
 
+function verificarLogin($val_email){
+  $email = mysqli_real_escape_string($conexao, $val_email);
+  $sql = "SELECT cd_login FROM tb_login WHERE cd_email = '$email' LIMIT 1;";
+  $resultado_usuario = mysqli_query($conexao, $sql);
+  $resultado = mysqli_fetch_assoc($resultado_usuario);
+  if(isset($resultado)){
+      return true;
+  }else{	
+      return false;
+   }
+}
+function verificarRG($val_rg){
+  $rg = mysqli_real_escape_string($conexao, $val_rg);
+  $sql = "SELECT cd_pessoa FROM tb_pessoa WHERE cd_rg = '$rg' LIMIT 1;";
+  $resultado_usuario = mysqli_query($conexao, $sql);
+  $resultado = mysqli_fetch_assoc($resultado_usuario);
+  if(isset($resultado)){
+      return true;
+  }else{	
+      return false;
+   }
+}
+function verificarCPF($val_cpf){
+  $cpf = mysqli_real_escape_string($conexao, $val_cpf);
+  $sql = "SELECT cd_pessoa FROM tb_pessoa WHERE cd_cpf = '$cpf' LIMIT 1;";
+  $resultado_usuario = mysqli_query($conexao, $sql);
+  $resultado = mysqli_fetch_assoc($resultado_usuario);
+  if(isset($resultado)){
+      return true;
+  }else{	
+      return false;
+   }
+}
 ?>
